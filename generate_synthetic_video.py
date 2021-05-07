b682a578617d1f64d62828ecb5f33a9b94b85794
Rudy <rudy.dev.kor@gmail.com>
2021-05-07 11:29:02 +0900

    Add generate num

--- generate_synthetic_video.py
+++ generate_synthetic_video.py
@@ -46,7 +46,7 @@ def generate_repeat_video(video_path,
                           repeat_speed_x_range=(0.5, 2),
                           output_fps=30,
                           video_size_wh=(224, 224),
-                          video_num=3):
+                          generate_num=3,
 
     """
     1. 영상 읽기
@@ -60,43 +60,53 @@ def generate_repeat_video(video_path,
 
     """
 
+
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     
-    clip_frame_size = round(random.uniform(*clip_sec_range) * fps)
-    padding_frame_size_s = round(random.uniform(*padding_sec_range) * fps)
-    padding_frame_size_e = round(random.uniform(*padding_sec_range) * fps)
-    frame_size = padding_frame_size_s + clip_frame_size + padding_frame_size_e
 
-    if frame_size > frame_count:
-        raise Exception('framesize is too large')
+    error_count = 0
+    for i in range(generate_num):
+        clip_frame_size = round(random.uniform(*clip_sec_range) * fps)
+        padding_frame_size_s = round(random.uniform(*padding_sec_range) * fps)
+        padding_frame_size_e = round(random.uniform(*padding_sec_range) * fps)
+        frame_size = padding_frame_size_s+clip_frame_size+padding_frame_size_e
 
-    start_frame = random.randint(0, frame_count-frame_size)
-
-    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-    frames = []
-    if cap.isOpened():
-        for _ in range(frame_size):
-            success, frame = cap.read()
-            if not success:
+        if frame_size > frame_count:
+            error_count += 1
+            print('{}: frame size is too large. (count {})'.format(
+                os.path.basename(video_path), error_count))
+                
+            if error_count >= 10:
                 break
+        
+        start_frame = random.randint(0, frame_count-frame_size)
 
-            # frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(frame, video_size_wh)
-            frames.append(frame)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        frames = []
+        if cap.isOpened():
+            for _ in range(frame_size):
+                success, frame = cap.read()
+                if not success:
+                    break
 
-    padding_s = frames[:padding_frame_size_s]
-    clip = frames[padding_frame_size_s:padding_frame_size_s+clip_frame_size]
-    padding_e = frames[padding_frame_size_s+clip_frame_size:]
+                # frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, video_size_wh)
+                frames.append(frame)
 
-    clip_repeat = generate_repeat_with_various_speed(
-        array=clip,
-        repeat_speed_x_range=repeat_speed_x_range,
-        repeat_num_range=repeat_num_range,
-        concat_result=True)
+        padding_s = frames[:padding_frame_size_s]
+        clip = frames[padding_frame_size_s:padding_frame_size_s+clip_frame_size]
+        padding_e = frames[padding_frame_size_s+clip_frame_size:]
 
-    result = concat_lists([padding_s, clip_repeat, clip, padding_e])
+        clip_repeat = generate_repeat_with_various_speed(
+            array=clip,
+            repeat_speed_x_range=repeat_speed_x_range,
+            repeat_num_range=repeat_num_range,
+            concat_result=True)
+
+        result = concat_lists([padding_s, clip_repeat, clip, padding_e])
+
 
     for frame in result:
         cv2.imshow('result', frame)
@@ -111,5 +121,5 @@ if __name__ == '__main__':
         repeat_speed_x_range=(0.8, 1.2),
         output_fps=30,
         video_size_wh=(854, 480),
-        video_num=3
+        generate_num=3,
     )
